48f04381b7007f36990fe9e5b2f9d278116097f3
roshan92 <r.sellvan@quamble.com.my>
2015-01-28 21:21:44 +0800

    create model sale

---
+++ db/migrate/20150128131930_create_sales.rb
@@ -0,0 +1,11 @@
+class CreateSales < ActiveRecord::Migration
+  def change
+    create_table :sales do |t|
+      t.datetime :date
+      t.string :code
+      t.decimal :value
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150128131930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "sales", force: true do |t|
+    t.datetime "date"
+    t.string   "code"
+    t.decimal  "value"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
