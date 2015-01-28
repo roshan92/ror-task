acb560cdef3fd110f8fdefcecbbe254408548c24
roshan92 <r.sellvan@quamble.com.my>
2015-01-29 00:16:15 +0800

    code cleanup

--- app/api/api.rb
+++ app/api/api.rb
@@ -3,17 +3,11 @@ class API < Grape::API
   version 'v1', using: :path
 
   rescue_from ActiveRecord::RecordNotFound do |e|
-    Rack::Response.new({
-      error_code: 404,
-      error_message: e.message,
-      }.to_json, 404).finish
+    Rack::Response.new({ error_code: 404, error_message: e.message }.to_json, 404).finish
   end
 
   rescue_from :all do |e|
-    Rack::Response.new({
-      error_code: 500,
-      error_message: e.message
-      }.to_json, 500).finish
+    Rack::Response.new({ error_code: 500, error_message: e.message }.to_json, 500).finish
   end
 
   mount V1::Sales
