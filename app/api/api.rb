4065074261bfdc12b04aa27cc3560a81aa7a1127
roshan92 <r.sellvan@quamble.com.my>
2015-01-29 00:05:06 +0800

    prefixing api/v1 call and mount V1::Sales class

---
+++ app/api/api.rb
@@ -0,0 +1,20 @@
+class API < Grape::API
+  prefix 'api'
+  version 'v1', using: :path
+
+  rescue_from ActiveRecord::RecordNotFound do |e|
+    Rack::Response.new({
+      error_code: 404,
+      error_message: e.message,
+      }.to_json, 404).finish
+  end
+
+  rescue_from :all do |e|
+    Rack::Response.new({
+      error_code: 500,
+      error_message: e.message
+      }.to_json, 500).finish
+  end
+
+  mount V1::Sales
+end
